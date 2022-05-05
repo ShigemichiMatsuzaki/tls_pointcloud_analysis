d360257aa4f7728e7411d923e61ac54a33e6ef1e
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2022-05-05 18:55:10 +0900

    Initial commit

---
+++ read_laz.py
@@ -0,0 +1,36 @@
+import pylas
+import open3d as o3d
+import numpy as np
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description='Visualization and analysis of LAS/LAZ point cloud files')
+
+    parser.add_argument('filename', type=str, help='Name of LAS/LAZ file')
+
+    return parser.parse_args()
+
+
+def main():
+    """ Main function
+    """
+    args = get_arguments()
+    file_name = args.filename
+    las = pylas.read(file_name)
+    print(las.points.size)
+    print(las.point_format.dimension_names)
+
+    np_points = np.array([las['X'], las['Y'], las['Z']]) / 1000.0
+
+    del las
+
+    o3d_points = o3d.geometry.PointCloud()
+    o3d_points.points = o3d.utility.Vector3dVector(np_points.T)
+    # pcd = las.points[:3, :]
+    o3d.visualization.draw_geometries([o3d_points])
+
+
+if __name__ == '__main__':
+    main()
